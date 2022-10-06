e3e96b831f654f25b1346316ab3c8c574180be71
Joao Vilela <jmdvilela@gmail.com>
2022-10-06 15:58:38 -0300

    wip costmap

--- src/costmap.py
+++ src/costmap.py
@@ -3,4 +3,19 @@
 
     script responsable for building the costmap
 """
+def costmap(path_to_raster, path_to_shape, cost):
+    
+    # open raster file
+    raster = open_raster(path_to_raster)
+
+    # open shapefile
+    shape  = open_shape(path_to_shape)
+
+    # add cost to raster according to the shapefile region
+    raster_mod = add_cost_to_raster(raster, shape, cost)
+
+    return raster_mod
+
+def add_cost_to_raster(raster, shape, cost):
+    return raster
 
