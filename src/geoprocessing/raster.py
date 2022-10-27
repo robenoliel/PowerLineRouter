426f31669b1ee48c26ecf0302cb39a54dd5892dd
robenoliel <robenoliel@gmail.com>
2022-10-27 16:35:23 -0300

    added protections for coordinate methods

--- src/geoprocessing/raster.py
+++ src/geoprocessing/raster.py
@@ -3,3 +3,11 @@
 
     script responsable for handling raster files 
 """
+
+def get_coordinates_from_xy(xy_list, base_raster):
+    """
+    Returns georeferenced coordinates of points in a raster according to its xy positions. Parameters:
+        xy_list: List of tuples
+        base_raster: Matrix position (int)
+    """
+    pass
